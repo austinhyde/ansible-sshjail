df3c2ba88665f592de8e3eff3af724a3840326ee
Austin Hyde <austin109@gmail.com>
2015-05-05 19:33:35 -0400

    Get sshjail working, and with variable jail owner

--- sshjail.py
+++ sshjail.py
@@ -14,15 +14,23 @@ class Connection(object):
     ''' jail-over-ssh based connections '''
 
     def get_jail_path(self):
-        code, _, stdout, stderr = self._exec_command(' '.join(['jls', '-j', self.jname, '-q', 'path']), '', 'root');
+        code, _, stdout, stderr = self._exec_command(' '.join(['jls', '-j', self.jname, '-q', 'path']), '', self.jailowner, True)
         # remove \n
+        return stdout.strip().split('\n')[-1]
+
+    def get_tmp_file(self):
+        code, _, stdout, stderr = self._exec_command('mktemp', '', None)
         return stdout.strip().split('\n')[-1]
  
         
     def __init__(self, runner, host, port, user, password, private_key_file, *args, **kwargs):
-        self.realhost = host
-        self.jname, self.host = host.split('@',1)
-        self.jname = re.sub(r'\W','_',self.jname)
+        self.orighost = host
+        jaildef, self.host = host.split('@',1)
+        jailowner = 'root'
+        if ':' in jaildef:
+            jaildef, jailowner = jaildef.split(':', 1)
+        self.jname = re.sub(r'\W','_',jaildef)
+        self.jailowner = jailowner
         self.runner = runner
         self.has_pipelining = False
         self.ssh = SSH.Connection(runner, self.host, port, user, password, private_key_file, *args)
@@ -32,20 +40,20 @@ class Connection(object):
         return self
 
     def _exec_command(self, cmd, tmp_path, become_user=None, sudoable=False, executable='/bin/sh', in_data=None):
-        vvv("JEXEC %s" % (cmd), host=self.realhost)
-        sys.stdin.readline()
-        return self.ssh.exec_command(cmd, tmp_path, 'root', True, executable, in_data)
+        # sys.stdin.readline()
+        return self.ssh.exec_command(cmd, tmp_path, become_user, sudoable, executable, in_data)
 
 
     def exec_command(self, cmd, tmp_path, become_user=None, sudoable=False, executable='/bin/sh', in_data=None):
-        ''' run a command on the chroot '''
+        ''' run a command in the jail '''
 
         if executable:
             local_cmd = ' '.join(['jexec', self.jname, executable, '-c', '"%s"' % cmd])
         else:
             local_cmd = 'jexec "%s" "%s"' % (self.jname, cmd)
 
-        return self._exec_command(local_cmd, tmp_path, become_user, sudoable, executable, in_data)
+        vvv("JAIL %s" % (local_cmd), host=self.orighost)
+        return self._exec_command(local_cmd, tmp_path, become_user, True, executable, in_data)
 
     def _normalize_path(self, path, prefix):
         if not path.startswith(os.path.sep):
@@ -55,14 +63,17 @@ class Connection(object):
 
     def put_file(self, in_path, out_path):
         ''' transfer a file from local to remote jail '''
+        tmp = self.get_tmp_file()
+        self.ssh.put_file(in_path, tmp)
         out_path = self._normalize_path(out_path, self.get_jail_path())
-        print out_path
-        self.ssh.put_file(in_path, out_path)
+        self._exec_command(' '.join(['mv',tmp,out_path]), '', self.jailowner, True)
 
     def fetch_file(self, in_path, out_path):
         ''' fetch a file from remote jail to local '''
+        tmp = self.get_tmp_file()
         in_path = self._normalize_path(in_path, self.get_jail_path())
-        self.ssh.fetch_file(in_path, out_path)
+        self._exec_command(' '.join(['mv',in_path,tmp]), '', self.jailowner, True)
+        self.ssh.fetch_file(tmp, out_path)
 
     def close(self):
         ''' terminate the connection; nothing to do here '''
